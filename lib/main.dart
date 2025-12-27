b5a7e45123a714d4c6e859924cc1ed06e88edbe3
officialanshika <officialsonianshika@gmail.com>
2025-12-27 22:29:32 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,263 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BMICalculatorApp());
+}
+
+class BMICalculatorApp extends StatelessWidget {
+  const BMICalculatorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'BMI Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: BMIScreen(),
+    );
+  }
+}
+
+
+class BMIScreen extends StatefulWidget {
+  const BMIScreen({super.key});
+
+  @override
+  State<BMIScreen> createState() => _BMIScreenState();
+}
+
+class _BMIScreenState extends State<BMIScreen> {
+
+  final TextEditingController heightController = TextEditingController();
+  final TextEditingController weightController = TextEditingController();
+  double? bmi;
+  String category = "";
+  String gender = "Male";
+
+
+  void calculateBMI(){
+    double height = double.parse(heightController.text)/100;
+    double weight = double.parse(weightController.text);
+
+    setState(() {
+      bmi = weight / pow(height, 2);
+
+      if(bmi! < 18.5){
+        category = "Under weight";
+      }else if(bmi! >= 18.5 && bmi! < 24.9){
+        category = "Normal";
+      }else if(bmi! >= 25 && bmi! < 29.9){
+        category = "Over weight";
+      }else{
+        category = "Obese";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 80, 78, 78),
+        title: Text('BMI Calculator', style: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),),
+        centerTitle: true,
+        elevation: 5,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color.fromARGB(198, 88, 80, 80),  Color.fromARGB(255, 126, 120, 120)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            )
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text('Enter Your Details',
+            style: TextStyle(
+              fontSize: 26,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+            textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20,),
+            _buildGenderToggle(),
+            SizedBox(height: 20,),
+            _buildInputFields(controller: heightController, label: "Height (cm)", icon: Icons.height),
+              SizedBox(height: 20,),
+            _buildInputFields(controller: weightController, label: "Weight (kg)", icon: Icons.monitor_weight_rounded),
+            SizedBox(height: 30,),
+            ElevatedButton(
+              onPressed: calculateBMI, 
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(vertical: 16),
+                backgroundColor: const Color.fromARGB(255, 137, 126, 126),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.circular(20),
+                ),
+                elevation: 8,
+              ),
+              child: Text("Calculate BMI", style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),),
+            ),
+
+            SizedBox(height: 30,),
+            if(bmi != null)  _buildResultCard(),
+           
+          ],
+        ),
+      ),
+    );
+  }
+
+
+  Widget _buildGenderToggle(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildGenderButton(
+              "Male",
+              Icons.male,
+              gender == "Male"
+            ),
+        SizedBox(width: 20,),
+        _buildGenderButton(
+              "Female",
+              Icons.female,
+              gender == "Female"
+            ),
+        
+      ],
+    );
+  }
+
+
+  Widget _buildGenderButton(String label, IconData icon, bool isSelected){
+    return GestureDetector(
+      onTap: (){
+        setState(() {
+          gender = label;
+        });
+      },
+      child: AnimatedContainer(
+        duration: Duration(microseconds: 300),
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+        decoration: BoxDecoration(
+          color: isSelected ? Colors.white : Colors.white.withAlpha(128),
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: isSelected
+          ? [const BoxShadow(color: Colors.black26, blurRadius: 5, offset: Offset(0, 3))]
+          : []
+        ),
+        child: Row(
+          children: [
+            Icon(icon, color: isSelected ? Colors.grey : Colors.white,),
+            SizedBox(width: 10,),
+            Text(label, 
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: isSelected ? Colors.grey : Colors.white
+            ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+  Widget _buildInputFields(
+    {
+        required TextEditingController controller,
+        required String label,
+        required IconData icon
+    }
+  ){
+    return TextField(
+      controller: controller,
+      keyboardType: TextInputType.number,
+      style: TextStyle(
+        color: Colors.white,
+      ),
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.white),
+        prefixIcon: Icon(icon, color: Colors.white,),
+        filled: true,
+        fillColor: Colors.white.withAlpha(128),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+          borderRadius: BorderRadius.circular(15)
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: const Color.fromARGB(255, 90, 89, 89)),
+          borderRadius: BorderRadius.circular(15)
+        )
+      ),
+    );
+  }
+
+
+  Widget _buildResultCard(){
+    return Card(
+      color: Colors.white.withAlpha(128),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadiusGeometry.circular(15),
+      ),
+      elevation: 10,
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Your BMI : ${bmi!.toStringAsFixed(2)}",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: const Color.fromARGB(255, 63, 60, 60),
+            ),
+            ),
+
+            SizedBox(height: 10,),
+
+            Text("Category : $category",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: bmi! < 18.5 
+              ? Colors.orange : (bmi! < 24.9 
+              ? Colors.green : (bmi! < 29.9 
+              ? Colors.orange : Colors.red)
+            ),
+            ),),
+
+            SizedBox(height: 10,),
+
+            Text("Gender : $gender",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: const Color.fromARGB(255, 63, 60, 60),
+            ),
+            ),
+          ],
+        ),
+        ),
+    );
+  }
+}
